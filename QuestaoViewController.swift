f0e3c3efaa0a2ae337aedbfa903e521678f7bdf9
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-08 21:09:57 -0300

    Commit - Correções

--- QuestaoViewController.swift
+++ QuestaoViewController.swift
@@ -8,36 +8,28 @@
 import UIKit
 
 class QuestaoViewController: UIViewController {
-
+    
     @IBOutlet weak var tituloQuestaoLabel: UILabel!
-     
-    @IBOutlet var botaoRespostas: [UIButton]!
-    @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
+    
+    @IBAction func respostabotaoPressionado(_ sender: UIButton) {
         print(sender.tag)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-        configurarLayout()
+        navigationItem.hidesBackButton = true
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
         
     }
-    
-    func configurarLayout() {
-        navigationItem.hidesBackButton = true
-        for botao in botaoRespostas {
-            botao.layer.cornerRadius = 12.0
-        }
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
