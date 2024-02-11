b886831a2a7a318b4a35851b4233c7e36e075558
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-11 18:13:45 -0300

    Commit - Colocando um timer entre questões01

--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -34,7 +34,8 @@ class QuestaoViewController: UIViewController {
         
         if numeroQuestao < questoes.count - 1   {
             numeroQuestao += 1
-            configurarQuestao()
+            Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(configurarQuestao), userInfo: nil, repeats: false)
+            //configurarQuestao()
         }
     }
     
@@ -47,7 +48,7 @@ class QuestaoViewController: UIViewController {
         }
     }
     
-    func configurarQuestao(){
+    @objc func configurarQuestao(){
         tituloQuestaoLabel.text = questoes[numeroQuestao].titulo
         for botao in botoesResposta {
             let tituloBotao = questoes[numeroQuestao].respostas[botao.tag]
